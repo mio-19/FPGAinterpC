3b437ef096f2bb56aadb802eefd3df2e321ac696
澪 <mio@mio19.uk>
2023-02-12 02:40:33 +0800

    StateT

--- hw/spinal/interpC/common/StateT.scala
+++ hw/spinal/interpC/common/StateT.scala
@@ -1,12 +1,21 @@
 package interpC.common
 
-import spinal.lib.fsm.StateMachineAccessor
+import spinal.lib.fsm._
 
 // Monad
 // describe actions in a state machine
 case class StateT[T](body: (((T => Unit), StateMachineAccessor) => Unit)) {
   def execute(implicit stateMachineAccessor: StateMachineAccessor): Unit = body({ ignored => () }, stateMachineAccessor)
 
+  def loop: StateMachine = new StateMachine {
+    val stateA: State = new State with EntryPoint {
+      whenIsActive {
+        body({ ignored => goto(stateA) }, implicitly[StateMachineAccessor])
+      }
+    }
+  }
+
   def map = ???
+
   def flatMap = ???
 }
